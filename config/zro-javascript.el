f18c9c98ef353b7a46c1ccbc60e0cecc290d6129
zrobisho <zrobisho21@gmail.com>
2015-05-31 09:53:45 -0400

    Updated js2-mode vars to behave better in node scripts

--- config/zro-javascript.el
+++ config/zro-javascript.el
@@ -1,5 +1,24 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; All js stuff
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js-mode-hook 'js2-minor-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; NODE.JS stuff
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
+;; setup global stuff
+(eval-after-load "js2-mode"
+  '(progn
+     (setq-default js2-basic-offset 4)
+     (setq-default js2-include-node-externs t)
+     (setq js2-skip-preprocessor-directives t)))
 
 (provide 'zro-javascript)
